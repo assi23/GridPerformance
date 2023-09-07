d595ade865d7687a60a9dfe77cf2ec16ffca23dd
Guilherme Assi <guiassi@hotmail.com>
2023-09-07 12:12:47 -0300

    feat: radient back color

--- GridPerformance/GridPerformance/MainWindow.xaml.cs
+++ GridPerformance/GridPerformance/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace GridPerformance
