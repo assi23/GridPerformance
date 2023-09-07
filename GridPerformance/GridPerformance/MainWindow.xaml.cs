7f6d52456597788ab6c617c358387e2a77dd4dfe
Guilherme Assi <guiassi@hotmail.com>
2023-09-06 21:48:07 -0300

    feat: validacoes

--- GridPerformance/GridPerformance/MainWindow.xaml.cs
+++ GridPerformance/GridPerformance/MainWindow.xaml.cs
@@ -36,11 +36,14 @@ namespace GridPerformance
 		}
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
+			SetarValores();
+
+			if (!Validacoes())
+				return;
+
 			base.Cursor = Cursors.Wait;
 
 			lbTempoProcessamento.Content = "Gerando linhas ...";
-
-			SetarValores();
 
 			LimparTabela();
 
@@ -56,6 +59,33 @@ namespace GridPerformance
 			lbTempoProcessamento.Content = "Tempo de processamento:" + dataFim.Subtract(dataInicio).Milliseconds + " ms";
 
 			base.Cursor = Cursors.Arrow;
+		}
+
+		private bool Validacoes()
+		{
+			lbErro.Content = "";
+			lbErro.Visibility = Visibility.Hidden;
+
+			if (valorMin <= 0 || valorMin >= valorMax)
+			{
+				lbErro.Visibility = Visibility.Visible;
+				lbErro.Content = "O valor mínimo não pode ser menor que 0 ou maior que o valor máximo!";
+			}
+			if (valorMax <= 0 || valorMax <= valorMin)
+			{
+				lbErro.Visibility = Visibility.Visible;
+				lbErro.Content += "O valor máximo não pode ser menor que 0 ou menor que o valor mínimo!";
+			}
+			if (qtdLinhasGerar <= 0)
+			{
+				lbErro.Visibility = Visibility.Visible;
+				lbErro.Content += "O número de linhas a ser gerado não pode ser menor que 0!";
+			}
+
+			if(!string.IsNullOrWhiteSpace(lbErro.Content.ToString()))
+				return false;
+
+			return true;
 		}
 
 		private void LimparTabela()
